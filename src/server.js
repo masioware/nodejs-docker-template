e6a66cb54a71d19c2dc87725cf8879077505b3b7
masioware <marcioedumartinez@gmail.com>
2022-09-28 14:29:49 -0300

    simplifying server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 
-app = require("./app");
-
+const app = require("./app");
 const port = process.env.PORT;
 
-app.listen(port, () => console.info("Backend listening on port:", port));
+app.listen(port);
