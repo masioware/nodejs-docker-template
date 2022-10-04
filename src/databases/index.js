8057630ee47a4d92e2dd5d2f5b6bdc90d53edea8
masioware <marcioedumartinez@gmail.com>
2022-10-04 20:53:41 -0300

    fix env and database/index.js to connect mysql

--- src/databases/index.js
+++ src/databases/index.js
@@ -2,6 +2,6 @@ const Sequelize = require("sequelize");
 
 const config = require("../config/database");
 
-const sequelize = new Sequelize(config);
+const sequelize = new Sequelize({ ...config, host: "mysql" });
 
 module.exports = { database: sequelize };
