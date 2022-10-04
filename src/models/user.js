8edfaa4f75fa511426741bca3cd6da4815ce3775
masioware <marcioedumartinez@gmail.com>
2022-10-04 16:08:24 -0300

    creating sequelize user model

---
+++ src/models/user.js
@@ -0,0 +1,22 @@
+const Sequelize = require("sequelize");
+
+const { database } = require("../databases");
+
+const User = database.define("users", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = User;
