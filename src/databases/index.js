40fd92c25b4c390b0888faf6fc43c18aef5afe21
masioware <marcioedumartinez@gmail.com>
2022-10-07 15:01:10 -0300

    fixing docker container host bug

--- src/databases/index.js
+++ src/databases/index.js
@@ -2,6 +2,6 @@ const Sequelize = require("sequelize");
 
 const config = require("../config/database");
 
-const sequelize = new Sequelize({ ...config, host: "mysql" });
+const sequelize = new Sequelize(config);
 
 module.exports = { database: sequelize };
